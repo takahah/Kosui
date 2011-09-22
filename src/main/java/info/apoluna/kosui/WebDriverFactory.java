2f6f9d90b27ef1617ebfc6309b9d841307b04582
Hideaki Takahashi <takahah@gmail.com>
2011-09-23 01:11:56 +0900

    add opera.

--- src/main/java/info/apoluna/kosui/WebDriverFactory.java
+++ src/main/java/info/apoluna/kosui/WebDriverFactory.java
@@ -4,12 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import com.opera.core.systems.OperaDriver;
+
 public class WebDriverFactory {
 	public static WebDriver getDriver(String browser) {
 		if ("firefox".equals(browser)) {
 			return new FirefoxDriver();
 		} else if ("ie".equals(browser)) {
 			return new InternetExplorerDriver();
+		} else if ("opera".equals(browser)) {
+			return new OperaDriver();
 		} else {
 			throw new RuntimeException("unsupported:" + browser);
 		}
